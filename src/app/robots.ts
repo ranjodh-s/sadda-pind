d9b6f1a7c4ddf755db4f76e89b9be78659e0d55e
Ranjodh Singh <rnjodh039@gmail.com>
2026-09-25 10:40:48 +0530

    added sitemap and robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,10 +1,11 @@
-export default function robots() {
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
     },
-
     sitemap: "https://sadda-pind.vercel.app/sitemap.xml",
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,4 +1,6 @@
-export default function sitemap() {
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://sadda-pind.vercel.app",
